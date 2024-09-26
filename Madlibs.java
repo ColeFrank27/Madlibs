4d5255a35bac3a914fcb52461dc62c76b1a2eac0
ColeFrank <cole.frank27@student.tamdistrict.org>
2024-09-26 09:35:13 -0700

    initialized scanner and created madlibs

---
+++ Madlibs.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class Madlibs {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        String madlib = "I went to the animal <noun>, the <plural_noun> and the <number> beasts were there.";
+        // madlib = "Once upon a time in a <adjective> land, a <noun> <adverb> grew.";
+        // madlib = "Many years ago there was a <adjective> <noun>, that liked to
+        // <verb>."
+
+    }
+
+}
